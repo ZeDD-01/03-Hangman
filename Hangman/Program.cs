243f7593a555e1c40daa524e168cfc45df2e3ddd
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-01-03 16:50:28 +0100

    Random Word chosen Compare user input with the letter in the given word print output on console

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,26 +4,39 @@ class Hangman
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Hangman");
         
+        //List that saves the words, that needs to be guessed
         List<String> hangmanWords = new List<string>();
         
         hangmanWords.Add("Garage");
         hangmanWords.Add("Bauzaun");
         hangmanWords.Add("Bratensoße");
+        hangmanWords.Add("Hemd");
         
-        List<String> meineListe = new List<string>();
+        //Randomly a word is being chosen
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(0, hangmanWords.Count-1);
         
-        meineListe.Add("Peter");
-        meineListe.Add("Sara");
-        meineListe.Add("Ute");
-        meineListe.Add("Hans");
-
-        foreach (string names in meineListe)
+        String chosenWord = hangmanWords[randomNumber];
+        Console.WriteLine("The word you have to guess has " + chosenWord.Length + " letters");
+        Console.Write("Choose a letter: ");
+        char userInput = Convert.ToChar(Console.ReadLine());
+        foreach (char letter in chosenWord)
         {
-            Console.WriteLine(names);
+            if (letter == userInput)
+            {
+                Console.Write(userInput + " ");
+            }
+            else
+            {
+                Console.Write("_ ");
+            }
+            
         }
         
+        
+            
         
     }
 }
