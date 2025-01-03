a09608a419d04bfb1bfb14e55f8162adb1a014b0
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-01-03 16:23:06 +0100

    Initial commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+class Hangman
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        List<String> hangmanWords = new List<string>();
+        
+        hangmanWords.Add("Garage");
+        hangmanWords.Add("Bauzaun");
+        hangmanWords.Add("Bratensoße");
+        
+        List<String> meineListe = new List<string>();
+        
+        meineListe.Add("Peter");
+        meineListe.Add("Sara");
+        meineListe.Add("Ute");
+        meineListe.Add("Hans");
+
+        foreach (string names in meineListe)
+        {
+            Console.WriteLine(names);
+        }
+        
+        
+    }
+}
