c018e2ea8fd4b2f219ae48ef46e1b0a3d823c62a
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-01-27 20:56:50 +0100

    reformatting the code, adding if-statement to check if the user already entered the character before

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,25 +1,25 @@
-﻿
-
-using System;
+﻿using System;
 
 class Hangman
 {
+    const int NUMBER_OF_GUESSES = 10;
+
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Hangman");
-        
+
         //List that saves the words, that needs to be guessed
         List<String> hangmanWords = new List<string>();
-        
+
         hangmanWords.Add("garage");
         hangmanWords.Add("bauzaun");
         hangmanWords.Add("bratensosse");
         hangmanWords.Add("hemd");
-        
+
         //Randomly a word is being chosen
         Random rnd = new Random();
-        int randomNumber = rnd.Next(0, hangmanWords.Count-1);
-        
+        int randomNumber = rnd.Next(0, hangmanWords.Count - 1);
+
         //An array is being created and filled with underscrore characters.
         String chosenWord = hangmanWords[randomNumber];
         Char[] letters = new Char[chosenWord.Length];
@@ -28,23 +28,23 @@ class Hangman
         {
             letters[i] = '_';
         }
-        
+
         Console.WriteLine("The word you have to guess has " + chosenWord.Length + " letters");
         Console.WriteLine(chosenWord);
-       
+
         int falseGuesses = 0;
         bool continueUserInput = true;
-        const int NUMBER_OF_GUESSES = 10;
-        
+
+
         //As long as the condition is true, the user can enter letters
         while (continueUserInput)
         {
             Console.Write("Choose a letter: ");
             ConsoleKeyInfo userInputKey = Console.ReadKey();
             char userInput = userInputKey.KeyChar;
-           
+
             bool rightGuessed = false;
-            
+
             if (chosenWord.Contains(userInput))
             {
                 Console.WriteLine("You got the letter! ");
@@ -52,33 +52,42 @@ class Hangman
                 {
                     if (chosenWord[i] == userInput)
                     {
-                        letters[i] = userInput;
+                        if (letters[i] != '_')
+                        {
+                            Console.WriteLine("You already had this letter! ");
+                        }
+                        else
+                        {
+                            letters[i] = userInput;
+                        }
+
                         rightGuessed = true;
                     }
                 }
             }
-            
+
             Console.WriteLine(string.Join(" ", letters));
 
             if (!rightGuessed)
-                {
-                    falseGuesses++;
-                    Console.WriteLine("This letter is not contained in the word. You can try " + (NUMBER_OF_GUESSES - falseGuesses) + " times");
-                }
+            {
+                falseGuesses++;
+                Console.WriteLine("This letter is not contained in the word. You can try " +
+                                  (NUMBER_OF_GUESSES - falseGuesses) + " times");
+            }
 
             if (!Array.Exists(letters, letter => letter == '_'))
-                {
-                    Console.WriteLine("Tadaaa. You guessed the word");
-                    continueUserInput = false;
-                    break;
-                }
+            {
+                Console.WriteLine("Tadaaa. You guessed the word");
+                continueUserInput = false;
+                break;
+            }
 
             if (falseGuesses >= NUMBER_OF_GUESSES)
-                {
-                    Console.WriteLine("You used too many tries. Sorry, you lost");
-                    continueUserInput = false;
-                    break;
-                }
+            {
+                Console.WriteLine("You used too many tries. Sorry, you lost");
+                continueUserInput = false;
+                break;
+            }
         }
     }
 }
